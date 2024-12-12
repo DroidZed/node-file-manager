9866029a3e027a8323773cfc88d5ab2623d7328a
DroidZed <41507665+DroidZed@users.noreply.github.com>
2024-12-12 21:13:29 +0100

    Update mainController.js

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,7 +3,7 @@ import { HOST, PORT } from '../config/env.js';
 export async function returnFileName(req, res) {
   res.status(201).json({
     message: `Uploaded`,
-    fileName: `${HOST}:${PORT}/api/static/${req.file?.filename}`,
+    fileName: `${HOST}/api/static/${req.file?.filename}`,
   });
 }
 
@@ -11,7 +11,7 @@ export async function returnFileNames(req, res) {
   res.status(201).json({
     message: `Uploaded`,
     fileNames: req.files?.map(
-      (f) => `${HOST}:${PORT}/api/static/${f?.filename}`
+      (f) => `${HOST}/api/static/${f?.filename}`
     ),
   });
 }
